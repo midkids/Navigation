44a409b4710b16174b556219f15ccdf8a727b931
midkids <83714759+midkids@users.noreply.github.com>
2026-06-06 20:44:11 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Myron Snelson on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
